aef03d1bc4e912f779be764f3f2352bb69d23ba5
jonichi <jonichi16@gmail.com>
2024-12-23 19:10:39 +0800

    feat: add controller unit test

--- service/auth/src/main/java/com/jonichi/peridot/auth/controller/AuthController.java
+++ service/auth/src/main/java/com/jonichi/peridot/auth/controller/AuthController.java
@@ -1,7 +1,10 @@
 package com.jonichi.peridot.auth.controller;
 
+import com.jonichi.peridot.auth.dto.AuthTokenDTO;
 import com.jonichi.peridot.auth.dto.RegisterRequestDTO;
+import com.jonichi.peridot.auth.service.AuthService;
 import com.jonichi.peridot.common.dto.ApiResponse;
+import com.jonichi.peridot.common.dto.SuccessResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -19,16 +22,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/api/auth")
+@RequiredArgsConstructor
 public class AuthController {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthController.class);
+    private final AuthService authService;
 
     /**
      * Registers a new user and returns an authentication accessToken.
      *
      * <p>This endpoint accepts a {@link RegisterRequestDTO} containing the user's
      * registration details (username, email, and password). It delegates the registration
-     * process to the {@link } and returns an authentication accessToken on success.</p>
+     * process to the {@link AuthService} and returns an authentication accessToken on success.</p>
      *
      * @param registerRequestDTO the registration request data containing username, email, and
      *     password
@@ -36,10 +41,33 @@ public class AuthController {
      *     accessToken
      */
     @PostMapping("/register")
-    public ResponseEntity<ApiResponse<?>> register(
+    public ResponseEntity<ApiResponse<AuthTokenDTO>> register(
             @RequestBody @Valid RegisterRequestDTO registerRequestDTO
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        logger.info("Start - Controller - register");
+        logger.debug(
+                "Request: username={}, email={}, password={}",
+                registerRequestDTO.username(),
+                registerRequestDTO.email(),
+                "******"
+        );
+
+        AuthTokenDTO authTokenDTO = authService.register(
+                registerRequestDTO.username(),
+                registerRequestDTO.email(),
+                registerRequestDTO.password()
+        );
+
+
+        HttpStatus status = HttpStatus.CREATED;
+        ApiResponse<AuthTokenDTO> response = SuccessResponse.<AuthTokenDTO>builder()
+                .code(status.value())
+                .message("User registered successfully")
+                .data(authTokenDTO)
+                .build();
+
+        logger.info("End - Controller - register");
+        return ResponseEntity.status(status).body(response);
     }
 
 }
