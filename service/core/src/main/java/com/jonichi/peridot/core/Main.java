b3f62fc6e2daeb04069ac2f5d48f642dc37011ea
jonichi <jonichi16@gmail.com>
2024-12-23 18:33:46 +0800

    feat: set up auth module

--- service/core/src/main/java/com/jonichi/peridot/core/Main.java
+++ service/core/src/main/java/com/jonichi/peridot/core/Main.java
@@ -2,7 +2,9 @@ package com.jonichi.peridot.core;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 /**
  * Main entry point for the application.
@@ -18,10 +20,16 @@ import org.springframework.context.annotation.ComponentScan;
  *   setup.</li>
  *   <li>{@link ComponentScan} - Specifies the base packages to scan for Spring-managed
  *   components.</li>
+ *   <li>{@link EnableJpaRepositories} - Enables Spring Data JPA repositories in the defined base
+ *  *   packages.</li>
+ *   <li>{@link EntityScan} - Configures the scanning of JPA entity classes within the specified
+ *   packages.</li>
  * </ul>
  */
 @SpringBootApplication
 @ComponentScan(basePackages = {"com.jonichi.peridot"})
+@EnableJpaRepositories(basePackages = {"com.jonichi.peridot"})
+@EntityScan(basePackages = {"com.jonichi.peridot"})
 public class Main {
 
     /**
