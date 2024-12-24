9bf3fe2f342439052e64ecb23f6dde1856a47e6c
jonichi <jonichi16@gmail.com>
2024-12-24 19:22:07 +0800

    feat: add Budget model and BudgetStatus enum

--- service/budget/src/main/java/com/jonichi/peridot/budget/repository/BudgetRepository.java
+++ service/budget/src/main/java/com/jonichi/peridot/budget/repository/BudgetRepository.java
@@ -1,4 +1,7 @@
 package com.jonichi.peridot.budget.repository;
 
-public interface BudgetRepository {
+import com.jonichi.peridot.budget.model.Budget;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface BudgetRepository extends JpaRepository<Budget, Integer> {
 }
--- service/budget/src/test/java/com/jonichi/peridot/budget/service/impl/BudgetServiceImplTest.java
+++ service/budget/src/test/java/com/jonichi/peridot/budget/service/impl/BudgetServiceImplTest.java
@@ -1,10 +1,12 @@
 package com.jonichi.peridot.budget.service.impl;
 
 import com.jonichi.peridot.auth.service.UserUtil;
+import com.jonichi.peridot.budget.model.Budget;
 import com.jonichi.peridot.budget.repository.BudgetRepository;
 import java.math.BigDecimal;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import static org.mockito.ArgumentMatchers.any;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import static org.mockito.Mockito.times;
@@ -26,13 +28,19 @@ public class BudgetServiceImplTest {
     public void createBudget_shouldCallUserUtilAndBudgetRepositoryOnce() throws Exception {
         // given
         BigDecimal amount = new BigDecimal("1000");
+        Budget budget = Budget.builder()
+                .userId(1)
+                .amount(amount)
+                .build();
 
         // when
         when(userUtil.getUserId()).thenReturn(1);
+        when(budgetRepository.save(budget)).thenReturn(budget);
         budgetService.createBudget(amount);
 
         // then
         verify(userUtil, times(1)).getUserId();
+        verify(budgetRepository, times(1)).save(budget);
     }
 
 }
