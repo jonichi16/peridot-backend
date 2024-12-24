7af04a5a0da547414ab954cb65c9b3f7ba61e64b
jonichi <jonichi16@gmail.com>
2024-12-24 23:38:31 +0800

    feat: add getCurrentBudget logic

--- service/budget/src/main/java/com/jonichi/peridot/budget/repository/BudgetRepository.java
+++ service/budget/src/main/java/com/jonichi/peridot/budget/repository/BudgetRepository.java
@@ -1,6 +1,9 @@
 package com.jonichi.peridot.budget.repository;
 
+import com.jonichi.peridot.budget.dto.BudgetDataDTO;
 import com.jonichi.peridot.budget.model.Budget;
+import java.time.LocalDate;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
@@ -12,4 +15,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
  *
  */
 public interface BudgetRepository extends JpaRepository<Budget, Integer> {
+
+    /**
+     * Retrieves the current budget for a given user and period.
+     *
+     * <p>This method fetches the user's budget for the specified period. If a budget exists
+     * for the provided user and period, it returns the associated data. If no budget is found,
+     * an empty {@link Optional} is returned.</p>
+     *
+     * @param userId the ID of the user whose budget is to be retrieved
+     * @param period the period for which the budget is requested
+     * @return an {@link Optional} containing a {@link BudgetDataDTO} with the current budget
+     *     details, or an empty {@link Optional} if no budget exists for the given user and period
+     */
+    Optional<BudgetDataDTO> getCurrentBudget(Integer userId, LocalDate period);
 }
