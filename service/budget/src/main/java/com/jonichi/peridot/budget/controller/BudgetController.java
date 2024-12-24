3ad9724204b1761451925498b97380dc80880ea8
jonichi <jonichi16@gmail.com>
2024-12-24 18:16:05 +0800

    feat: add BudgetController

--- service/budget/src/main/java/com/jonichi/peridot/budget/controller/BudgetController.java
+++ service/budget/src/main/java/com/jonichi/peridot/budget/controller/BudgetController.java
@@ -1,7 +1,14 @@
 package com.jonichi.peridot.budget.controller;
 
+import com.jonichi.peridot.budget.dto.BudgetResponseDTO;
 import com.jonichi.peridot.budget.dto.CreateBudgetDTO;
+import com.jonichi.peridot.budget.service.BudgetService;
+import com.jonichi.peridot.common.dto.ApiResponse;
+import com.jonichi.peridot.common.dto.SuccessResponse;
 import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -11,13 +18,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/budgets")
+@RequiredArgsConstructor
 public class BudgetController {
 
+    private static final Logger logger = LoggerFactory.getLogger(BudgetController.class);
+    private final BudgetService budgetService;
+
     @PostMapping("")
-    public ResponseEntity<?> createBudget(
+    public ResponseEntity<ApiResponse<BudgetResponseDTO>> createBudget(
             @RequestBody @Valid CreateBudgetDTO createBudgetDTO
     ) {
-        return ResponseEntity.status(HttpStatus.CREATED).build();
+        logger.info("Start - Controller - createBudget");
+        logger.debug(
+                "Request: amount={}",
+                createBudgetDTO.amount()
+        );
+
+        BudgetResponseDTO budgetResponseDTO = budgetService.createBudget(createBudgetDTO.amount());
+
+        HttpStatus status = HttpStatus.CREATED;
+        ApiResponse<BudgetResponseDTO> response = SuccessResponse.<BudgetResponseDTO>builder()
+                .code(status.value())
+                .message("Budget created successfully")
+                .data(budgetResponseDTO)
+                .build();
+
+        logger.info("End - Controller - createBudget");
+        return ResponseEntity.status(status).body(response);
     }
 
 }
--- service/budget/src/test/java/com/jonichi/peridot/budget/controller/BudgetControllerTest.java
+++ service/budget/src/test/java/com/jonichi/peridot/budget/controller/BudgetControllerTest.java
@@ -1,4 +1,63 @@
 package com.jonichi.peridot.budget.controller;
 
+import com.jonichi.peridot.budget.dto.BudgetResponseDTO;
+import com.jonichi.peridot.budget.dto.CreateBudgetDTO;
+import com.jonichi.peridot.budget.service.BudgetService;
+import com.jonichi.peridot.common.dto.ApiResponse;
+import java.math.BigDecimal;
+import java.util.Objects;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.ResponseEntity;
+
+@ExtendWith(MockitoExtension.class)
 public class BudgetControllerTest {
+
+    @Mock
+    private BudgetService budgetService;
+    @InjectMocks
+    private BudgetController budgetController;
+
+    @Test
+    public void createBudget_shouldCallBudgetServiceOnce() throws Exception {
+        // given
+        CreateBudgetDTO createBudgetDTO = CreateBudgetDTO.builder()
+                .amount(new BigDecimal("1000"))
+                .build();
+
+        // when
+        budgetController.createBudget(createBudgetDTO);
+
+        // then
+        verify(budgetService, times(1)).createBudget(createBudgetDTO.amount());
+    }
+    
+    @Test
+    public void createBudget_shouldReturnCorrectResponse() throws Exception {
+        // given
+        CreateBudgetDTO createBudgetDTO = CreateBudgetDTO.builder()
+                .amount(new BigDecimal("1000"))
+                .build();
+        BudgetResponseDTO budgetResponseDTO = BudgetResponseDTO.builder()
+                .budgetId(1)
+                .build();
+        
+        // when
+        when(budgetService.createBudget(createBudgetDTO.amount())).thenReturn(budgetResponseDTO);
+        ResponseEntity<ApiResponse<BudgetResponseDTO>> response = budgetController.createBudget(createBudgetDTO);
+        
+        // then
+        assertThat(response.getStatusCode().value()).isEqualTo(201);
+        assertThat(Objects.requireNonNull(response.getBody()).getData().budgetId()).isEqualTo(1);
+        assertThat(response.getBody().getMessage()).isEqualTo("Budget created successfully");
+        assertThat(response.getBody().getCode()).isEqualTo(201);
+    }
+
 }
