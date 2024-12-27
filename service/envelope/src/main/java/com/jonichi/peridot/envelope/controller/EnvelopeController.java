270c91a98e1ef2db6f501dbfa0120aa1b9d39ae6
jonichi <jonichi16@gmail.com>
2024-12-27 15:57:21 +0800

    feat: add controller for creating envelopes

--- service/envelope/src/main/java/com/jonichi/peridot/envelope/controller/EnvelopeController.java
+++ service/envelope/src/main/java/com/jonichi/peridot/envelope/controller/EnvelopeController.java
@@ -1,6 +1,10 @@
 package com.jonichi.peridot.envelope.controller;
 
+import com.jonichi.peridot.common.dto.ApiResponse;
+import com.jonichi.peridot.common.dto.SuccessResponse;
 import com.jonichi.peridot.envelope.dto.CreateEnvelopeDTO;
+import com.jonichi.peridot.envelope.dto.EnvelopeResponseDTO;
+import com.jonichi.peridot.envelope.service.EnvelopeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
@@ -18,14 +22,31 @@ import org.springframework.web.bind.annotation.RestController;
 public class EnvelopeController {
 
     private static final Logger logger = LoggerFactory.getLogger(EnvelopeController.class);
+    private final EnvelopeService envelopeService;
 
     @PostMapping("")
-    public ResponseEntity<?> createEnvelope(
+    public ResponseEntity<ApiResponse<EnvelopeResponseDTO>> createEnvelope(
             @RequestBody @Valid CreateEnvelopeDTO createEnvelopeDTO
     ) {
         logger.info("Start - Controller - createBudget");
+        logger.debug("Request: {}", createEnvelopeDTO);
+
+        EnvelopeResponseDTO envelopeResponseDTO = envelopeService.createEnvelope(
+                createEnvelopeDTO.name(),
+                createEnvelopeDTO.description(),
+                createEnvelopeDTO.amount(),
+                createEnvelopeDTO.recurring()
+        );
+
+        HttpStatus status = HttpStatus.CREATED;
+        ApiResponse<EnvelopeResponseDTO> response = SuccessResponse.<EnvelopeResponseDTO>builder()
+                .code(status.value())
+                .message("Envelope created successfully")
+                .data(envelopeResponseDTO)
+                .build();
+
         logger.info("End - Controller - createBudget");
-        return ResponseEntity.status(HttpStatus.OK).build();
+        return ResponseEntity.status(status).body(response);
     }
 
 }
