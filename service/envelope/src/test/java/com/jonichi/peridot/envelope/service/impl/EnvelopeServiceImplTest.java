12e7b81a02bbe69f1bbd93d40b28261afca663a0
jonichi <jonichi16@gmail.com>
2024-12-27 19:32:20 +0800

    feat: add create envelopes logic

--- service/envelope/src/test/java/com/jonichi/peridot/envelope/service/impl/EnvelopeServiceImplTest.java
+++ service/envelope/src/test/java/com/jonichi/peridot/envelope/service/impl/EnvelopeServiceImplTest.java
@@ -1,11 +1,19 @@
 package com.jonichi.peridot.envelope.service.impl;
 
+import com.jonichi.peridot.budget.model.Budget;
+import com.jonichi.peridot.budget.service.BudgetContextService;
+import com.jonichi.peridot.common.dto.UserBudgetDTO;
+import com.jonichi.peridot.common.model.SystemStatus;
 import com.jonichi.peridot.common.util.TransactionalHandler;
 import com.jonichi.peridot.envelope.dto.EnvelopeResponseDTO;
+import com.jonichi.peridot.envelope.model.BudgetEnvelope;
+import com.jonichi.peridot.envelope.model.BudgetEnvelopeStatus;
+import com.jonichi.peridot.envelope.model.Envelope;
 import com.jonichi.peridot.envelope.repository.BudgetEnvelopeRepository;
 import com.jonichi.peridot.envelope.repository.EnvelopeRepository;
 import java.math.BigDecimal;
 import java.util.function.Supplier;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import static org.mockito.ArgumentMatchers.any;
@@ -24,6 +32,8 @@ public class EnvelopeServiceImplTest {
     @Mock
     private BudgetEnvelopeRepository budgetEnvelopeRepository;
     @Mock
+    private BudgetContextService budgetContextService;
+    @Mock
     private TransactionalHandler transactionalHandler;
     @InjectMocks
     private EnvelopeServiceImpl envelopeServiceImpl;
@@ -38,10 +48,42 @@ public class EnvelopeServiceImplTest {
                 .envelopeId(1)
                 .budgetEnvelopeId(1)
                 .build();
+        Envelope envelope = Envelope.builder()
+                .id(1)
+                .build();
+        BudgetEnvelope budgetEnvelope = BudgetEnvelope.builder()
+                .id(1)
+                .build();
+
+        Envelope envelopeRequest = Envelope.builder()
+                .userId(1)
+                .name(name)
+                .description(description)
+                .status(SystemStatus.SYSTEM_STATUS_ACTIVE)
+                .build();
+        BudgetEnvelope budgetEnvelopeRequest = BudgetEnvelope.builder()
+                .budgetId(1)
+                .envelopeId(1)
+                .amount(amount)
+                .recurring(true)
+                .status(BudgetEnvelopeStatus.ENVELOPE_STATUS_UNDER)
+                .build();
 
         // when
-        when(transactionalHandler.runInTransactionSupplier(any(Supplier.class))).thenReturn(envelopeResponseDTO);
-        envelopeServiceImpl.createEnvelope(
+        when(budgetContextService.getCurrentUserBudgetId()).thenReturn(
+                UserBudgetDTO.builder()
+                        .budgetId(1)
+                        .userId(1)
+                        .build()
+        );
+        when(envelopeRepository.save(envelopeRequest)).thenReturn(envelope);
+        when(budgetEnvelopeRepository.save(budgetEnvelopeRequest)).thenReturn(budgetEnvelope);
+        when(transactionalHandler.runInTransactionSupplier(any(Supplier.class)))
+                .thenAnswer(invocation -> {
+                    Supplier<Budget> supplier = invocation.getArgument(0);
+                    return supplier.get();
+                });
+        EnvelopeResponseDTO response = envelopeServiceImpl.createEnvelope(
                 name,
                 description,
                 amount,
@@ -49,6 +91,11 @@ public class EnvelopeServiceImplTest {
         );
 
         // then
+        verify(budgetContextService, times(1)).getCurrentUserBudgetId();
         verify(transactionalHandler, times(1)).runInTransactionSupplier(any(Supplier.class));
+        verify(envelopeRepository, times(1)).save(envelopeRequest);
+        verify(budgetEnvelopeRepository, times(1)).save(budgetEnvelopeRequest);
+        assertThat(response.envelopeId()).isEqualTo(1);
+        assertThat(response.budgetEnvelopeId()).isEqualTo(1);
     }
 }
