db075da08246f8a444576d06d9a436d7ebc5a370
jonichi <jonichi16@gmail.com>
2024-12-27 14:42:09 +0800

    feat: add test for error response

--- service/envelope/src/main/java/com/jonichi/peridot/envelope/controller/EnvelopeController.java
+++ service/envelope/src/main/java/com/jonichi/peridot/envelope/controller/EnvelopeController.java
@@ -1,11 +1,14 @@
 package com.jonichi.peridot.envelope.controller;
 
+import com.jonichi.peridot.envelope.dto.CreateEnvelopeDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,7 +20,9 @@ public class EnvelopeController {
     private static final Logger logger = LoggerFactory.getLogger(EnvelopeController.class);
 
     @PostMapping("")
-    public ResponseEntity<?> createEnvelope() {
+    public ResponseEntity<?> createEnvelope(
+            @RequestBody @Valid CreateEnvelopeDTO createEnvelopeDTO
+    ) {
         logger.info("Start - Controller - createBudget");
         logger.info("End - Controller - createBudget");
         return ResponseEntity.status(HttpStatus.OK).build();
