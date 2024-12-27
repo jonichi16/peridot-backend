52e4181d493380bd0131aeb7f4bf743999f975c3
jonichi <jonichi16@gmail.com>
2024-12-27 16:08:13 +0800

    docs: add javadocs to create envelopes

--- service/envelope/src/main/java/com/jonichi/peridot/envelope/dto/CreateEnvelopeDTO.java
+++ service/envelope/src/main/java/com/jonichi/peridot/envelope/dto/CreateEnvelopeDTO.java
@@ -8,6 +8,19 @@ import jakarta.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import lombok.Builder;
 
+/**
+ * Data Transfer Object for creating a new envelope.
+ *
+ * <p>Includes fields for the envelope's name, description, amount, and whether it is recurring.
+ * Input validation is applied to ensure that required fields are provided and adhere to
+ * specific constraints.</p>
+ *
+ * @param name        the name of the envelope. Must not be empty.
+ * @param description an optional description of the envelope.
+ * @param amount      the amount for the envelope. Must not be null, must be greater than or equal
+ *                    to 0, and must have at most 10 integer digits and 2 fractional digits.
+ * @param recurring   indicates if the envelope is recurring. Must not be null.
+ */
 @JsonIgnoreProperties(ignoreUnknown = true)
 @Builder
 public record CreateEnvelopeDTO(
--- service/envelope/src/main/java/com/jonichi/peridot/envelope/service/EnvelopeService.java
+++ service/envelope/src/main/java/com/jonichi/peridot/envelope/service/EnvelopeService.java
@@ -3,7 +3,26 @@ package com.jonichi.peridot.envelope.service;
 import com.jonichi.peridot.envelope.dto.EnvelopeResponseDTO;
 import java.math.BigDecimal;
 
+/**
+ * Service interface for managing envelopes.
+ *
+ * <p>Provides operations for creating and managing envelopes, including methods to handle
+ * envelope creation and other related functionalities.</p>
+ */
 public interface EnvelopeService {
+
+    /**
+     * Creates a new envelope.
+     *
+     * <p>This method creates a new envelope with the provided name, description, amount,
+     * and recurrence status. It returns the details of the created envelope.</p>
+     *
+     * @param name        the name of the envelope. Must not be null or empty.
+     * @param description an optional description of the envelope.
+     * @param amount      the amount allocated to the envelope. Must be greater than or equal to 0.
+     * @param recurring   indicates whether the envelope is recurring.
+     * @return a {@link EnvelopeResponseDTO} containing the details of the created envelope.
+     */
     EnvelopeResponseDTO createEnvelope(
             String name,
             String description,
