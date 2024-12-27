29785ccc98a8b630e2bc2f7f831e5abbc5cc9311
jonichi <jonichi16@gmail.com>
2024-12-27 19:32:20 +0800

    feat: add create envelopes logic

--- service/envelope/src/main/java/com/jonichi/peridot/envelope/service/impl/EnvelopeServiceImpl.java
+++ service/envelope/src/main/java/com/jonichi/peridot/envelope/service/impl/EnvelopeServiceImpl.java
@@ -1,7 +1,15 @@
 package com.jonichi.peridot.envelope.service.impl;
 
+import com.jonichi.peridot.budget.service.BudgetContextService;
+import com.jonichi.peridot.common.dto.UserBudgetDTO;
+import com.jonichi.peridot.common.model.SystemStatus;
 import com.jonichi.peridot.common.util.TransactionalHandler;
 import com.jonichi.peridot.envelope.dto.EnvelopeResponseDTO;
+import com.jonichi.peridot.envelope.model.BudgetEnvelope;
+import com.jonichi.peridot.envelope.model.BudgetEnvelopeStatus;
+import com.jonichi.peridot.envelope.model.Envelope;
+import com.jonichi.peridot.envelope.repository.BudgetEnvelopeRepository;
+import com.jonichi.peridot.envelope.repository.EnvelopeRepository;
 import com.jonichi.peridot.envelope.service.EnvelopeService;
 import java.math.BigDecimal;
 import java.util.function.Supplier;
@@ -12,6 +20,9 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 public class EnvelopeServiceImpl implements EnvelopeService {
 
+    private final BudgetContextService budgetContextService;
+    private final EnvelopeRepository envelopeRepository;
+    private final BudgetEnvelopeRepository budgetEnvelopeRepository;
     private final TransactionalHandler transactionalHandler;
 
     @Override
@@ -21,11 +32,30 @@ public class EnvelopeServiceImpl implements EnvelopeService {
             BigDecimal amount,
             Boolean recurring
     ) {
+        UserBudgetDTO userBudgetDTO = budgetContextService.getCurrentUserBudgetId();
 
-        Supplier<EnvelopeResponseDTO> supplier = () -> EnvelopeResponseDTO.builder().build();
+        Supplier<EnvelopeResponseDTO> supplier = () -> {
+            Envelope envelope = envelopeRepository.save(Envelope.builder()
+                    .userId(userBudgetDTO.userId())
+                    .name(name)
+                    .description(description)
+                    .status(SystemStatus.SYSTEM_STATUS_ACTIVE)
+                    .build());
 
-        EnvelopeResponseDTO envelopeResponseDTO = transactionalHandler.runInTransactionSupplier(supplier);
+            BudgetEnvelope budgetEnvelope = budgetEnvelopeRepository.save(BudgetEnvelope.builder()
+                    .budgetId(userBudgetDTO.budgetId())
+                    .envelopeId(envelope.getId())
+                    .amount(amount)
+                    .recurring(recurring)
+                    .status(BudgetEnvelopeStatus.ENVELOPE_STATUS_UNDER)
+                    .build());
 
-        return null;
+            return EnvelopeResponseDTO.builder()
+                    .envelopeId(envelope.getId())
+                    .budgetEnvelopeId(budgetEnvelope.getId())
+                    .build();
+        };
+
+        return transactionalHandler.runInTransactionSupplier(supplier);
     }
 }
