465c052d15e95ab75b1bd79c68fcb97a58bad1b3
jonichi <jonichi16@gmail.com>
2024-12-27 20:47:20 +0800

    docs: add javadocs

--- lib/common/src/main/java/com/jonichi/peridot/common/model/SystemStatus.java
+++ lib/common/src/main/java/com/jonichi/peridot/common/model/SystemStatus.java
@@ -1,5 +1,11 @@
 package com.jonichi.peridot.common.model;
 
+/**
+ * Enum representing the possible system statuses.
+ *
+ * <p>This enum defines the status of a system entity, indicating whether it is active or
+ * inactive.</p>
+ */
 public enum SystemStatus {
     SYSTEM_STATUS_ACTIVE,
     SYSTEM_STATUS_INACTIVE
--- service/envelope/src/main/java/com/jonichi/peridot/envelope/repository/EnvelopeRepository.java
+++ service/envelope/src/main/java/com/jonichi/peridot/envelope/repository/EnvelopeRepository.java
@@ -3,5 +3,12 @@ package com.jonichi.peridot.envelope.repository;
 import com.jonichi.peridot.envelope.model.Envelope;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+/**
+ * Repository interface for managing {@link Envelope} entities.
+ *
+ * <p>This interface extends {@link JpaRepository} to provide CRUD operations for
+ * {@link Envelope} entities. It allows interaction with the database for performing
+ * operations such as saving, deleting, and querying envelope data.</p>
+ */
 public interface EnvelopeRepository extends JpaRepository<Envelope, Integer> {
 }
