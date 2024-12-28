4abb5726f481dac128b3f143fcce59ea2df6eb88
jonichi <jonichi16@gmail.com>
2024-12-28 18:03:59 +0800

    feat: add query to update budget status repository

--- service/envelope/src/main/java/com/jonichi/peridot/envelope/repository/BudgetEnvelopeRepository.java
+++ service/envelope/src/main/java/com/jonichi/peridot/envelope/repository/BudgetEnvelopeRepository.java
@@ -1,7 +1,11 @@
 package com.jonichi.peridot.envelope.repository;
 
 import com.jonichi.peridot.envelope.model.BudgetEnvelope;
+import java.math.BigDecimal;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 /**
  * Repository interface for managing {@link BudgetEnvelope} entities.
@@ -10,5 +14,24 @@ import org.springframework.data.jpa.repository.JpaRepository;
  * {@link BudgetEnvelope} entities. It allows interaction with the database for performing
  * operations such as saving, deleting, and querying budget envelope data.</p>
  */
+@Repository
 public interface BudgetEnvelopeRepository extends JpaRepository<BudgetEnvelope, Integer> {
+
+    /**
+     * Retrieves the total expenses for a specific budget.
+     *
+     * <p>This method calculates the sum of all amounts in the {@code BudgetEnvelope}
+     * entities associated with the given budget ID.</p>
+     *
+     * @param budgetId the unique identifier of the budget.
+     * @return the total expenses as a {@code BigDecimal}.
+     */
+    @Query("""
+            SELECT SUM(be.amount)
+            FROM BudgetEnvelope be
+            WHERE be.budgetId = :budgetId
+            """)
+    BigDecimal getTotalExpenses(
+            @Param("budgetId") Integer budgetId
+    );
 }
