f59fe6cc298513dff7c5da3a400e8cc64b4d64fd
jonichi <jonichi16@gmail.com>
2024-12-29 10:59:12 +0800

    feat: add update envelope controller

--- service/envelope/src/test/java/com/jonichi/peridot/envelope/controller/EnvelopeControllerTest.java
+++ service/envelope/src/test/java/com/jonichi/peridot/envelope/controller/EnvelopeControllerTest.java
@@ -64,4 +64,45 @@ public class EnvelopeControllerTest {
         assertThat(response.getBody().getCode()).isEqualTo(201);
     }
 
+    @Test
+    public void updateEnvelope_shouldReturnTheCorrectResponse() throws Exception {
+        // given
+        BigDecimal amount = new BigDecimal("1000");
+        CreateUpdateEnvelopeDTO createUpdateEnvelopeDTO = CreateUpdateEnvelopeDTO.builder()
+                .name("Sample")
+                .description("This is sample envelope")
+                .amount(amount)
+                .recurring(true)
+                .build();
+        EnvelopeResponseDTO envelopeResponseDTO = EnvelopeResponseDTO.builder()
+                .envelopeId(1)
+                .budgetEnvelopeId(1)
+                .build();
+
+        // when
+        when(envelopeService.updateEnvelope(
+                1,
+                createUpdateEnvelopeDTO.name(),
+                createUpdateEnvelopeDTO.description(),
+                createUpdateEnvelopeDTO.amount(),
+                createUpdateEnvelopeDTO.recurring()
+        )).thenReturn(envelopeResponseDTO);
+        ResponseEntity<ApiResponse<EnvelopeResponseDTO>> response = envelopeController
+                .updateEnvelope(1, createUpdateEnvelopeDTO);
+
+        // then
+        verify(envelopeService, times(1)).updateEnvelope(
+                1,
+                createUpdateEnvelopeDTO.name(),
+                createUpdateEnvelopeDTO.description(),
+                createUpdateEnvelopeDTO.amount(),
+                createUpdateEnvelopeDTO.recurring()
+        );
+        assertThat(response.getStatusCode().value()).isEqualTo(200);
+        assertThat(Objects.requireNonNull(response.getBody()).getData().envelopeId()).isEqualTo(1);
+        assertThat(Objects.requireNonNull(response.getBody()).getData().budgetEnvelopeId()).isEqualTo(1);
+        assertThat(response.getBody().getMessage()).isEqualTo("Envelope updated successfully");
+        assertThat(response.getBody().getCode()).isEqualTo(200);
+    }
+
 }
