9d8c27bd0cb0da1a573b8581ee8a8ed349c91e54
jonichi <jonichi16@gmail.com>
2024-12-29 15:46:12 +0800

    feat: add get envelopes endpoint and integration test

--- service/envelope/src/main/java/com/jonichi/peridot/envelope/controller/EnvelopeController.java
+++ service/envelope/src/main/java/com/jonichi/peridot/envelope/controller/EnvelopeController.java
@@ -11,6 +11,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -109,6 +110,11 @@ public class EnvelopeController {
 
         logger.info("End - Controller - updateEnvelope");
         return ResponseEntity.status(status).body(response);
+    }
+
+    @GetMapping("/{budgetId}")
+    public ResponseEntity<?> getEnvelopes() {
+        return ResponseEntity.status(HttpStatus.OK).build();
     }
 
 }
